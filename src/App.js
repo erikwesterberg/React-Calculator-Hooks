ef36037a618908c445aa878f64b5d0568ab5b926
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-11 17:38:07 +0200

    create an array with some payments, also install uuid

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseItem from "./components/ExpenseItem";
 
+const initialExpenses = [
+  {id:1, charge: "Rent", amount: 1600},
+  {id:2, charge: "Car payment", amount: 600},
+  {id:3, charge: "Forest taxes", amount: 800}
+]
+console.log(initialExpenses)
 const App = () => {
   return (
     <>
     <Alert />
     <ExpenseForm />
     <ExpenseList />
+    <ExpenseItem />
     </>
   );
 };
