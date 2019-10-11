e8af3e4c6cfe9b4ddbb67b01884d82949a67927c
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-11 18:12:31 +0200

    maping trough the expenses props and render each object as a item, also import a icon to use with button

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Item from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
 
-
-const ExpenseList = () => {
+const ExpenseList = ({expenses}) => {
   return (
-    <div>
-      This is the list
-      <Item />
-    </div>
+    <>
+    <ul className="list">
+      {expenses.map((expense) => {
+        return <Item key={expense.id} expense={expense}/>
+      })}
+    </ul>
+    {expenses.length > 0 && (
+    <button className="btn">
+      Clear expenses
+    <MdDelete className="btn-icon" />
+    </button>)}
+    </>
   )
 }
 
