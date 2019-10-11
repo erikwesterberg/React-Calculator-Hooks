94085e6b9f43b5529537e5dc981b5e5406c55f70
erka¨git config --global user.name erkagit config --global user.name erka¨ <erik.westerberg@hotmail.se>
2019-10-11 19:16:20 +0200

    declare more state values inside of app, also creates functions to handele form actions and sends them down to expesnseForm as props

--- src/App.js
+++ src/App.js
@@ -7,28 +7,46 @@ import ExpenseItem from "./components/ExpenseItem";
 import uuid from "uuid/v4";
 
 const initialExpenses = [
-  {id: uuid(), charge: "Rent", amount: 1600},
-  {id: uuid(), charge: "Car payment", amount: 600},
-  {id: uuid(), charge: "Forest taxes", amount: 800}
-]
+  { id: uuid(), charge: "Rent", amount: 1600 },
+  { id: uuid(), charge: "Car payment", amount: 600 },
+  { id: uuid(), charge: "Forest taxes", amount: 800 }
+];
 
 const App = () => {
-  const [ expenses, setExpenses ] = useState(initialExpenses)
+  // State values
+  //, all expense, add expense
+  const [expenses, setExpenses] = useState(initialExpenses);
+  //Singe expense
+  const [charge, setCharge] = useState("");
+  //Singe amount
+  const [amount, setAmount] = useState("");
+  //functionality
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = e => {
+    setCharge(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <>
-    <Alert />
-    <h1>Budget Calculator</h1>
-    <main className="App">
-    <ExpenseForm />
-    <ExpenseList expenses={expenses}/>
-    </main>
-    <h1>
-      total spending : <span className="total">
-        ${expenses.reduce((acc, curr) => {
-          return (acc += curr.amount);
-        }, 0)}
-      </span>
-    </h1>
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm charge={charge} amount={amount} handleAmount={handleAmount} handleCharge={handleCharge}  handleSubmit={handleSubmit}
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
     </>
   );
 };
